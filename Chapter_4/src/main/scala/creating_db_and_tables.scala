e08758aafcd2b001e667028f8b89b75ee32f95c9
Gurmancheema <gurmanjotsingh97@gmail.com>
2026-02-27 04:29:18 -0500

    Created managed & unmanaged tables

--- Chapter_4/src/main/scala/creating_db_and_tables.scala
+++ Chapter_4/src/main/scala/creating_db_and_tables.scala
@@ -59,9 +59,45 @@ object creating_db_tables {
     // now again check the databases & tables inside it
     // it should have 2 tables by now
     // One created from SparkSQL, other created from Dataframe API
-
+  
     spark.sql("SHOW DATABASES").show()
     spark.sql("SHOW TABLES").show()
+
+    //query the empty table
+    spark.sql("""SELECT * FROM managed_us_flights_delays""").show()
+
+    // 4. Creating an unmanaged/external table from datasource
+    // creating using sparksql
+    // since i am passing the data file path's variable, SparkSQL needs a SQL string to parse the path
+    // therefore, i will interpolate the path stored in "data_file" variable
+
+    println(new java.io.File(".").getAbsolutePath)
+
+    spark.sql(s""" CREATE TABLE IF NOT EXISTS unmanaged_us_flight_delays(date STRING, delay INT, distance INT, origin                                                                           STRING, destination STRING)
+                  USING csv OPTIONS ('PATH' '$data_file') """)
+
+    // verify table creating in db
+    spark.sql("""SHOW TABLES""")
+
+    // creating unmanaged/external table using Dataframe API
+    // since we are giving the path of table storage;
+    // it falls in unmanaged/external table category
+
+    val df_2 = spark.read.format("csv").option("header","true").schema(schema).load(data_file)
+
+    df_2.write.mode("overwrite").option("path","tmp/data/us_flights_delays")
+                                .saveAsTable("df_api_unmanaged_us_flights_delays")
+
+    // verify table existence in database
+    // verify the type of table
+  
+   spark.sql("DESCRIBE EXTENDED df_api_unmanaged_us_flights_delays").show(false) 
+   spark.sql("""SHOW TABLES""").show()
+    
+
+
+    
+
   
 
 
