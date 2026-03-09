8b6cabdb238bae108e96e002dda1148ff096d6ff
Gurmancheema <gurmanjotsingh97@gmail.com>
2026-03-09 00:27:57 -0400

    complete uptil UNION operation

--- Chapter_5/src/main/scala/common_dataframe_sparksql_operations.scala
+++ Chapter_5/src/main/scala/common_dataframe_sparksql_operations.scala
@@ -4,6 +4,18 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
+
+//***************************** EXERCISE OBJECTIVE OVERVIEW ***************************************
+//
+//  1. Import two files and create two DataFrames, one for airport (airportsna) information
+//      and one for US flight delays (departureDelays).
+//
+//  2. Using expr(), convert the delay and distance columns from STRING to INT.
+//
+//  3. Create a smaller table, foo, that we can focus on for our demo examples; it contains
+//      only information on three flights originating from Seattle (SEA) to the destination
+//      of San Francisco (SFO) for a small time range.
+
 // defining a singleton scala object containing the main method
 
 object df_operations{
@@ -60,6 +72,53 @@ object df_operations{
 
     delayed_flights_df.show()
     delayed_flights_df.printSchema()
+
+    // column datatype conversion
+    // "distance" column from STRING to INT
+
+    val updated_flights_df = delayed_flights_df.withColumn("distance",col("distance").cast("int"))
+
+
+    // ***************************** PART-2 CREATING TEMPORARY VIEWS FROM THE DATASETS ****************************
+    
+    val foo = updated_flights_df.filter((col("origin") === "SEA")
+                                        && (col("destination") === "SFO")
+                                        && (col("date").like("1010%"))
+                                        && (col("delay") > 0))
+
+    foo.show()
+    println(s"Shape: (${foo.count()}, ${foo.columns.length})")
+
+
+    // ******************************* PART-3 COMMON OPERATIONS *****************************************
+    
+    //  1. ----------------------- UNION OPERATION --------------------------------------
+    
+    // union two dataframes with same schema together into one dataframe
+
+    val union_flight_dfs = updated_flights_df.union(foo)
+    union_flight_dfs.show()
+
+    println(s"Shape of resulting dataframe after union: ,(${union_flight_dfs.count()}, ${union_flight_dfs.columns.length})")
+
+
+   // verifying the union of dataframes by other way
+   // creating a new subset dataframe following the same conditions of foo dataframe
+
+   val verify_df =  union_flight_dfs.filter((col("origin") === "SEA") &&
+                                     (col("destination") === "SFO") &&
+                                     (col("date").like("1010%")) &&
+                                     (col("delay") > 0))
+                                    .distinct()
+
+   verify_df.show()
+
+
+
+
+
+
+
 
 
     // stop the spark session
